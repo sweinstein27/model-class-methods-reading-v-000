29570c42e73f68a2c31966b19bd3a852b4bbff0a
Scott Weinstein <sweinstein27@gmail.com>
2018-09-25 14:55:05 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,8 @@ class PostsController < ApplicationController
 
  # filter the @posts list based on user input
  if !params[:author].blank?
-   @posts = Post.where(author: params[:author])
- elsif !params[:date].blank?
-   if params[:date] == "Today"
-     @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
-   else
-     @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
-   end
- else
+  @posts = Post.by_author(params[:author])
+elsif !params[:date].blank?
    # if no filters are applied, show all posts
    @posts = Post.all
  end
